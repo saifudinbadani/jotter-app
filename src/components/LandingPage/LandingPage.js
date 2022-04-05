fe41120444bbc2b79d121909a618d789b0dd6b3f
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2022-04-05 14:01:29 +0800

    feat:landing page content added

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,10 @@
+import './LandingPage.css';
+
+ export const LandingPage = () => {
+    return <div className='landingPage bg-clr'>
+        <h1 className='title-landingPage heading-1'>Jotter</h1>
+        <p className='heading-2'>Note taking now made easy..</p>
+        <button className='btn btn-l btn-solid-secondary'>Login</button>
+        <a href="#" class="link secondary-link">Already have an account</a>
+    </div>
+}
