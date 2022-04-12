dd3363a83a6990f2f57b527655eca08a2ff92f2c
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2022-04-12 14:49:29 +0800

    fear: change color in note card added

--- src/components/HomePage/MainContent.js
+++ src/components/HomePage/MainContent.js
@@ -10,17 +10,23 @@ export const MainContent = () => {
     const [title, setTitle ] = useState('');
     const [content, setContent ] = useState('');
     const { noteState:{ notesData }, noteDispatch } = useNotes();
-    const [color, setColor] = useState('white');
+    const [colorSection, setColorSection] = useState(false);
     
-//   update color > onClick color =value > payload
+    
+    const bgColor = ['red', 'yellow', 'green', 'pink', 'purple']
+    
+    const randomBgClr = bgColor[Math.floor(Math.random()*bgColor.length)]
+    const clrChangeHandler = (currentNote, color) => {
+        const updatedNote = {...currentNote, color: color}
+        noteDispatch({type: 'COLOR_CHANGE', payload: updatedNote}) 
+    }
     const addThisNote = () => {
         noteDispatch({type: 'ADD_NOTE', payload:{ 
             title,
             content,
             date: `${new Date(Date.now()).toLocaleDateString()}`,
             id: uuidv4(),
-            color
-
+            color: randomBgClr
         }})
         setTitle('')
         setContent('')
@@ -38,19 +44,23 @@ export const MainContent = () => {
 
 
 
-
-
-
         <div className='notes-container display-flex'>
             
             {notesData.map((note) => {
 
-               return <div className='note display-flex' key={note.id}>
+            return <div className={`note display-flex ${note.color}`} key={note.id}>
                 <h3 className='title-note heading-3 m-1'>{note.title}</h3>
                 <p className='content-note heading-4 m-1'>{note.content}</p>
                 <p className='heading-5 m-1'>Created on {note.date}</p>
+               {colorSection && <div className='color-section'> 
+                    <button className='clr-btn btn red' onClick={() => clrChangeHandler(note, 'red')}/>
+                    <button className='clr-btn btn yellow' onClick={() => clrChangeHandler(note, 'yellow')}/>
+                    <button className='clr-btn btn green' onClick={() => clrChangeHandler(note, 'green')}/>
+                    <button className='clr-btn btn pink' onClick={() => clrChangeHandler(note, 'pink')}/>
+                    <button className='clr-btn btn purple'onClick={() => clrChangeHandler(note, 'purple')}/>
+                </div>}
                 <div className='notes-btn'>
-                    <i className="notes-icon fas fa-palette"></i>
+                    <i className="notes-icon fas fa-palette" onClick={() => setColorSection((prev) => !prev)}></i>
                     <i className="notes-icon fas fa-tag"></i>
                     <i className="notes-icon fas fa-archive" onClick={() => noteDispatch({type: 'ARCHIVE_NOTE', payload: note})}></i>
                     <i className="notes-icon fas fa-trash" onClick={() => noteDispatch({type: 'DELETE_NOTE', payload: note})}></i>
--- src/reducer/NotesReducer.js
+++ src/reducer/NotesReducer.js
@@ -1,5 +1,7 @@
 
 
+
+
  export const notesReducer = (state, {type, payload}) => {
     switch (type) {
         case 'DELETE_NOTE':
@@ -12,9 +14,21 @@
                  })]
             }        
         case 'ADD_NOTE':
+            
             return {
-                ...state, notesData: [...state.notesData, payload]
+                ...state, notesData: [...state.notesData, payload],
             }   
+        case 'COLOR_CHANGE':
+            return {
+                ...state, notesData: [...state.notesData.map((item) => {
+                    if(item.id === payload.id){
+                        return payload
+                    }else{
+                        return item
+                    }
+                })]
+            }    
+            
         case 'ARCHIVE_NOTE':
             return {
                 ...state, notesData: [...state.notesData.filter((item) => {
