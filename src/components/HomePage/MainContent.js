8b10baeb29fa3620dc73df00e2a11bc383eda426
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2022-04-13 17:15:14 +0800

    feat: priority and label added

--- src/components/HomePage/MainContent.js
+++ src/components/HomePage/MainContent.js
@@ -8,6 +8,9 @@ import './Homepage.css';
 export const MainContent = ({setModalOpen}) => {
     const [title, setTitle ] = useState('');
     const [content, setContent ] = useState('');
+    const [label, setLabel] = useState('');
+    const [priority, setPriority] = useState('low');
+
     const { noteState:{ notesData }, noteDispatch } = useNotes();
     const bgColor = ['red', 'yellow', 'green', 'pink', 'purple']
     const randomBgClr = bgColor[Math.floor(Math.random()*bgColor.length)]
@@ -17,17 +20,30 @@ export const MainContent = ({setModalOpen}) => {
             content,
             date: `${new Date(Date.now()).toLocaleDateString()}`,
             id: uuidv4(),
-            color: randomBgClr
+            color: randomBgClr,
+            label,
+            priority
+
         }})
         setTitle('')
         setContent('')
+        setLabel('')
     }
     return <div className='main-notes-container display-flex'>
         <div className='input-note-container display-flex p-1 heading-3'>
             <input type='text' name='title' className='title-input-note heading-3 m-1' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='title comes here...'/>
             <textarea name='content' className='content-input-note m-1' placeholder='content comes here...'  onChange={(e) => setContent(e.target.value) } value={content}/>
-            <div className='notes-btn'>
-                    <i className="notes-icon fas fa-tag"></i>
+            <div className='notes-btn display-flex'>
+                    <div className='priority-input-note heading-4'>
+                    <label for="priority-select">Priority: </label>
+
+                    <select name="priority" id="priority-select" onChange={(e) => setPriority(e.target.value)}>
+                        <option value="Low" >Low</option>
+                        <option value="Medium">Medium</option>
+                        <option value="High">High</option>
+                    </select>
+                    </div>
+                    <input type='text' name='label' className='label-input-note heading-4 m-1' value={label} onChange={(e) => setLabel(e.target.value)} placeholder='label'/>
                     <button className='btn btn-solid-primary' onClick={() => addThisNote()}>Save</button>
                 </div>
         </div>
